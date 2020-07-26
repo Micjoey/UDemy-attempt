9d967f1a237fe5e321289e38a1f198f3f856ac18
Macallan Savett <jsavett@gmail.com>
2020-07-26 09:40:10 -0700

    add Question class with question and answer fields

--- Udemy-Quizzes/math_quiz/models.py
+++ Udemy-Quizzes/math_quiz/models.py
@@ -1,3 +1,6 @@
+from django.utils import timezone
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    question = models.CharField(max_length=200, min_length=2)
+    answer = models.IntegerField()
